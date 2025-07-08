34b0a7c7883563d440738632c08e16b5f69372b9
Phatlom-Lss <Chayanoon5k@gmail.com>
2025-07-08 13:23:06 +0700

    middleware add

---
+++ src/app/main/middleware.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('token');
+  if (!token && request.nextUrl.pathname.startsWith('/main')) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+  return NextResponse.next();
+}
